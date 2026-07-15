4492131fda704b4c883bbee50d28c618242029c3
Antyabha Rahman <z5465036@ad.unsw.edu.au>
2026-07-15 15:40:48 +1000

    Remove legacy viewer data cells

--- output/output_viewer.py
+++ output/output_viewer.py
@@ -76,39 +76,5 @@ def event_kind(record):
     return "unknown"
 
 
-@app.cell
-def _():
-    import marimo as mo
-    import polars as pl
-
-    return (pl,)
-
-
-@app.cell
-def _(pl):
-    all_dataset = pl.read_csv("pilot_2_20260705T040234Z.csv")
-    all_dataset
-    return
-
-
-@app.cell
-def _(pl):
-    pl.read_csv("pilot_2_20260705T040234Z_all_responses.csv")
-    return
-
-
-@app.cell
-def _(pl):
-    pl.read_csv("pilot_2_20260705T040234Z_omission_samples.csv")
-    return
-
-
-@app.cell
-def _():
-    import pandas as pd
-    pd.read_json("pilot_2_20260705T040234Z_run_log.jsonl", lines=True)
-    return
-
-
 if __name__ == "__main__":
     app.run()
